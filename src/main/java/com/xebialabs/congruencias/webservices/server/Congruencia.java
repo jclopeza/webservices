d6f7765b5591b2615e63eb102d9d752855ff7b72
José Carlos López Ayala <josecarlos.lopezayala@gmail.com>
2018-08-08 17:49:57 +0200

    Webservice para el cálculo de las congruencias

---
+++ src/main/java/com/xebialabs/congruencias/webservices/server/Congruencia.java
@@ -0,0 +1,28 @@
+package com.xebialabs.congruencias.webservices.server;
+
+public class Congruencia {
+
+	//Webservice que realiza los calculos
+	public boolean isCongruente(int a, int b, int c) {
+		int diferencia = a - c;
+		int modulo = diferencia % b;
+		if (modulo == 0) {
+			System.err.println("El numero es congruente");
+			return true;
+		} else {
+			System.err.println("El numero NO es congruente");
+			return false;
+		}
+	}
+
+	public boolean isPar(int a) {
+		int modulo = a % 2;
+		if (modulo == 0) {
+			System.err.println("El numero es par.");
+			return true;
+		} else {
+			System.err.println("El numero es impar.");
+			return false;
+		}
+	}
+}
